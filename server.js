cd75be1034cb7709e1ba6e63c8c11827b130b07e
David Kipkemboi <davidkla12@gmail.com>
2021-02-18 16:31:20 +0300

    init

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 hbs.registerPartials(__dirname + '/views/partials');
@@ -8,10 +9,20 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/assets'));
 app.use((req, res, next)=>{
     var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log',log + '\n', (err) => {
+        if(err){
+            console.log('Unable to append file to server.log' + err);
+        }
+    });
     console.log(`${now}: ${req.method} ${req.url}`);
     next();
 
 });
+
+// app.use((req, res, next)=>{
+// res.render('maintenance');
+// });
 
 hbs.registerHelper('getCurrentYear',()=>{
     return new Date().getFullYear();
